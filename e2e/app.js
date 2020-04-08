6fabacbbc0477e45830d494f443d81b75243bad5
Eddie Ekpo <eddie@eddies-mbp.lan1>
2020-04-07 20:23:23 -0700

    Initializaiton and setup of the different components of the App. MongoDB can be setup online with MongoDB Atlas. The Node initialization is in server.js and express initialization is in e2e/app.js.
    If you want to insert anything html into the app, just put it inside src/HTMLSTUFFHERE.
    
    Signed-off-by: Eddie Ekpo <eekpo1@github.com>

---
+++ e2e/app.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+
+const Post = require('./models/post');
+
+const app = express();
+
+// We should put the mongoose connection here
+
+
+// JSON PARSER - Body Parser
+app.use(bodyParser.json())
+
+// These headers help us make requests from Node to angular through CORS
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, DELETE, PUT, OPTIONS'
+  );
+  next();
+});
+
---
+++ server.js
@@ -0,0 +1,54 @@
+/* Our Node Settings. We work with express in ./e2e/app.js */
+
+const http = require('http');
+const app = require('./e2e/app');
+const debug = require('debug')('node.angular');
+
+const normalizePort = val => {
+  let port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+}
+
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+
+  const bind = typeof addr === "string" ? `pipe: ${addr}` : `port ${port}`;
+  switch (error.code) {
+    case "EACCES":
+      console.error(`${bind} requires elevated privileges`);
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(`${bind} is already in use`);
+      process.exit(1);
+      break;
+    default:
+      throw error
+  }
+};
+
+const onListening = () => {
+  const addr = server.address;
+  const bind = typeof addr === "string" ? `pipe ${addr}` : `port ${addr}`;
+  debug(`Listening on ${bind}`);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set('port', port);
+
+const server = http.createServer(app);
+
+server.listen(port);
